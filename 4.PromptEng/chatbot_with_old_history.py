2faa26f87f6803b2840764f12a208ae6d9e31f54
muhammadmaaz724 <muhammadmaaz724@gmail.com>
2025-06-20 22:58:09 +0500

    Learned how to add previous history to chat

--- 4.PromptEng/chatbot_with_old_history.py
+++ 4.PromptEng/chatbot_with_old_history.py
@@ -12,17 +12,20 @@ model = ChatHuggingFace(
     llm=llm
 )
 
-model = ChatHuggingFace(
-    llm=llm
-)
+chat_template = ChatPromptTemplate([
+    ('system','you are a helpfull assistant'),
+    MessagesPlaceholder(variable_name="chat_history"),
+    ('human','{query}')
+    ])
+
+chat_history = []
 
 with open("chat_history.txt") as file:
     chat_history.extend(file.readlines())
     
-    
 prompt = chat_template.invoke({
     'chat_history':chat_history,
-    'query': "where am i from?"
+    'query': "how many houses do i have?"
 })
 
 res = model.invoke(prompt)
